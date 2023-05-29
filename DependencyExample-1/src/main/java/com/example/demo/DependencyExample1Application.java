c3da8f36a67800445af2e9139fa295817ea890b1
Mr-Adesh <36359386+Mr-Adesh@users.noreply.github.com>
2023-05-29 22:00:12 +0530

    simpleWeb

---
+++ DependencyExample-1/src/main/java/com/example/demo/DependencyExample1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependencyExample1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DependencyExample1Application.class, args);
+	}
+
+}
